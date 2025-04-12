3396705b337235851aba56ba3dee314236ca7116
Paul Tome <120785679+Clavius4@users.noreply.github.com>
2025-04-12 14:24:10 +0300

    Clavius: Registered all services in the service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,22 +2,27 @@
 
 namespace App\Providers;
 
+use App\Services\AuthService;
+use App\Services\UserService;
+use App\Services\RoleService;
+use App\Services\PermissionService;
+use App\Services\Contracts\AuthServiceInterface;
+use App\Services\Contracts\UserServiceInterface;
+use App\Services\Contracts\RoleServiceInterface;
+use App\Services\Contracts\PermissionServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->bind(AuthServiceInterface::class, AuthService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(RoleServiceInterface::class, RoleService::class);
+        $this->app->bind(PermissionServiceInterface::class, PermissionService::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
         //
     }
