999f3d4d4d60708e57fe529dd082311360de80d5
Lyatuujacquisdo <152864459+Lyatuujacquisdo@users.noreply.github.com>
2025-04-13 12:28:02 +0300

    Added database configuration in .env file, and integrated template vue notus, also added Home.Vue

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Landing />
+</template>
+
+<script setup>
+import Landing from '@/notus/views/Landing.vue'
+</script>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Route::get('/', function () {
+//     return Inertia::render('Home');
+// })->name('home');
+
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return Inertia::render('Home');
+});
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
